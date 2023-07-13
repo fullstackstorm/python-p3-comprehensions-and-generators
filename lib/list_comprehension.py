f20775ddd502297581ffc957d07a4a206a1e3127
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-07-13 15:50:23 -0700

    Practice list comprehensions.

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens_list = [n for n in num_list if n % 2 == 0]
+    return evens_list
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_list = []
+    for sentence in sentence_list:
+        exclamation_list.append(sentence + '!')
+    return exclamation_list
